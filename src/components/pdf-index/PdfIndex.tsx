1d0d8943b23cb29670b2d73f98100b3f9377e245
sooyang <pppppark2@gmail.com>
2023-01-05 21:11:33 +0900

    feat #14 -pdf상세페이지 컴포넌트 분리

--- src/components/pdf-index/PdfIndex.tsx
+++ src/components/pdf-index/PdfIndex.tsx
@@ -1,10 +1,12 @@
 import dummy from '../../db/data.json';
 import PdfIndexBox from './PdfIndexBox';
 import styles from './PdfIndex.module.css';
+import PdfDetail from './pdf_detail/PdfDetail';
 
 export default function PdfIndex() {
     return (
         <ul className={styles.container}>
+            <PdfDetail/>
             {dummy.circles.map((circle) =>
                 <li key={circle.id}>
                     <PdfIndexBox circle={circle.circle} />
