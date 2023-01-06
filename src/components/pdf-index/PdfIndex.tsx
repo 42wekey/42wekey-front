533e700976b517e2196f2860194123e1ea340693
sooyang <pppppark2@gmail.com>
2023-01-06 16:04:57 +0900

    feat #17 - pdf-index컴포넌트 rest api 코드로 수정

--- src/components/pdf-index/PdfIndex.tsx
+++ src/components/pdf-index/PdfIndex.tsx
@@ -1,13 +1,25 @@
-import dummy from '../../db/data.json';
 import PdfIndexBox from './PdfIndexBox';
 import styles from './PdfIndex.module.css';
-import PdfDetail from './pdf_detail/PdfDetail';
+import { useEffect, useState } from 'react';
+
+export interface PdfIdx {
+    id: number;
+    circle: number;
+};
 
 export default function PdfIndex() {
+    const [circle, setCircle] = useState<PdfIdx[]>([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/circles`)
+            .then(res => res.json())
+            .then(data => setCircle(data));
+    }, []);
+
     return (
         <ul className={styles.container}>
-            {dummy.circles.map((circle) =>
-                <li key={circle.id}>
+            {circle.map((circle) =>
+                <li key={circle.circle}>
                     <PdfIndexBox circle={circle.circle} />
                 </li>
             )}
