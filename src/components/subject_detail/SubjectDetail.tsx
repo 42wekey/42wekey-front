c0366a498e9e46d40ed87082ad3f0acc1ba56454
sooyang <pppppark2@gmail.com>
2023-02-27 19:21:40 +0900

    feat #32 progerss bar 추가

--- src/components/subject_detail/SubjectDetail.tsx
+++ src/components/subject_detail/SubjectDetail.tsx
@@ -9,11 +9,11 @@ import SubjectWiki from "./SubjectWiki";
 import SubjectInfo from "./SubjectInfo";
 import Graph from "../graph/Graph";
 
-interface intraId{
-  intraId:String;
+interface intraId {
+  intraId: String;
 }
 
-export default function SubjectDetail({intraId}:intraId) {
+export default function SubjectDetail({ intraId }: intraId) {
   const [scroll, setScroll] = useState(0);
   const maxScroll = getMaxScroll();
 
@@ -21,7 +21,7 @@ export default function SubjectDetail({intraId}:intraId) {
     const { scrollHeight, offsetHeight } = document.documentElement;
     return Math.max(scrollHeight, offsetHeight) - window.innerHeight;
   }
-  const params = useParams()as {circle: string, sbj_name: string}; //params  = {subject : sbj_name}
+  const params = useParams() as { circle: string; sbj_name: string }; //params  = {subject : sbj_name}
   // const {circle, sbj_name} = params
 
   function onScroll() {
@@ -30,45 +30,49 @@ export default function SubjectDetail({intraId}:intraId) {
 
   useEffect(() => {
     console.log("ScrollY is ", scroll); // ScrollY가 변화할때마다 값을 콘솔에 출력
-  }, [scroll])
+  }, [scroll]);
 
-  useEffect(()=>{
-    console.log(params)
-  },[]);
+  useEffect(() => {
+    console.log(params);
+  }, []);
 
-  useEffect(()=>{
-    const watch = () => {
-      window.addEventListener('scroll', onScroll);
-    }
-    watch(); // addEventListener 함수를 실행
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
     return () => {
-      window.removeEventListener('scroll', onScroll);
-    }
-  },[scroll]);
-  
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, [scroll]);
+
   return (
     <div className={styles.pdf}>
-      <Menu 
-      intraId = {'him'}/>
+      <Menu intraId={"him"} />
       <div className={styles.progress_bar}>
-        <div className={styles.progerss_bar_fill} style={{width:`${100*(scroll/maxScroll) + 1}%`}}></div>
+        <div
+          className={styles.progerss_bar_fill}
+          style={{ width: `${100 * (scroll / maxScroll)}%` }}
+        ></div>
       </div>
-      <SubjectHeader
-      info={{circle: params.circle, sbj_name: params.sbj_name}}
-      />
-      <div>
-      <SubjectInfo />
-      </div>
-      <div className={styles.SubjectWiki}>
-      <SubjectWiki />
-      </div>
-      <div className={styles.content}>
-      <CommentInput />
-      <SubjectComment 
-      intraId={intraId}/>
-      </div>
-      <div>
-        <Graph time_taken="a_week" difficulty="normal" amount_study="middle" /> 
+      <div className={styles.container}>
+        <SubjectHeader
+          info={{ circle: params.circle, sbj_name: params.sbj_name }}
+        />
+        <div>
+          <SubjectInfo />
+        </div>
+        <div className={styles.SubjectWiki}>
+          <SubjectWiki />
+        </div>
+        <div className={styles.content}>
+          <CommentInput />
+          <SubjectComment intraId={intraId} />
+        </div>
+        <div>
+          <Graph
+            time_taken="a_week"
+            difficulty="normal"
+            amount_study="middle"
+          />
+        </div>
       </div>
     </div>
   );
