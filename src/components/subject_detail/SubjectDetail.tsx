08964304f88b18aa149b80e59aac879df85b2cba
sooyang <pppppark2@gmail.com>
2023-02-27 19:02:18 +0900

    feat #32 progress bar 추가

--- src/components/subject_detail/SubjectDetail.tsx
+++ src/components/subject_detail/SubjectDetail.tsx
@@ -4,7 +4,7 @@ import SubjectComment from "./SubjectComment";
 import Menu from "../menu/Menu";
 import CommentInput from "../comment_input/CommentInput";
 import { useParams } from "react-router";
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import SubjectWiki from "./SubjectWiki";
 import SubjectInfo from "./SubjectInfo";
 import Graph from "../graph/Graph";
@@ -14,16 +14,45 @@ interface intraId{
 }
 
 export default function SubjectDetail({intraId}:intraId) {
+  const [scroll, setScroll] = useState(0);
+  const maxScroll = getMaxScroll();
+
+  function getMaxScroll() {
+    const { scrollHeight, offsetHeight } = document.documentElement;
+    return Math.max(scrollHeight, offsetHeight) - window.innerHeight;
+  }
   const params = useParams()as {circle: string, sbj_name: string}; //params  = {subject : sbj_name}
   // const {circle, sbj_name} = params
+
+  function onScroll() {
+    setScroll(Math.floor(window.scrollY));
+  }
+
+  useEffect(() => {
+    console.log("ScrollY is ", scroll); // ScrollY가 변화할때마다 값을 콘솔에 출력
+  }, [scroll])
+
   useEffect(()=>{
     console.log(params)
-  },[])
+  },[]);
+
+  useEffect(()=>{
+    const watch = () => {
+      window.addEventListener('scroll', onScroll);
+    }
+    watch(); // addEventListener 함수를 실행
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    }
+  },[scroll]);
   
   return (
     <div className={styles.pdf}>
       <Menu 
       intraId = {'him'}/>
+      <div className={styles.progress_bar}>
+        <div className={styles.progerss_bar_fill} style={{width:`${100*(scroll/maxScroll) + 1}%`}}></div>
+      </div>
       <SubjectHeader
       info={{circle: params.circle, sbj_name: params.sbj_name}}
       />
