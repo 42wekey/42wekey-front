7da05df4b9697dd769028cf2b4f0d6432d4ba8a3
sooyang <pppppark2@gmail.com>
2023-03-06 15:14:24 +0900

    feat #43 login

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 import { RecoilRoot } from "recoil";
 import Main from './components/Main';
+import Login from "./components/login/Login";
 import MyComment from "./components/myComment/MyComment";
 import SubjectDetail from './components/subject_detail/SubjectDetail';
 
 const App = () => {
+  const [isLogged, setIsLogged] = useState(false);
+
   return (
 
     <RecoilRoot>
     <Router>
     <Routes>
-      <Route path="/" element={<Main />} />
+      <Route path="/" element={isLogged===false?<Login />:<Main />} />
       {/* <Route path="subjectList" element={<Main />} /> */}
       <Route path="/:circle/:sbj_name" element={<SubjectDetail/>} />
       <Route path="/allComment/:intraId" element={<MyComment/>} />
