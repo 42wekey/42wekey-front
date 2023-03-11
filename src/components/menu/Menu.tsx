ddf9f5b8ae5edb77c6e57e543ea9e917df9aba54
HyeongwooIM <hyeongwoo1279@gmail.com>
2023-03-11 23:43:09 +0900

    임시 커밋

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -2,44 +2,45 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import styles from "./Menu.module.css";
 import { AsyncHook } from "async_hooks";
-import SearchBar from "./SearchBar";
 import DehazeIcon from "@mui/icons-material/Dehaze";
 import { dividerClasses } from "@mui/material";
 
-interface intraId{
-  intraId:String;
+interface intraId {
+  intraId: String;
 }
 
-interface subject{
+interface subject {
   id: Number;
   circle: Number;
   sbj_name: String;
 }
 
-export default function Menu({intraId}:intraId) {
-  const [search, setSearch] =useState<string>('');
+export default function Menu({ intraId }: intraId) {
+  const [search, setSearch] = useState<string>("");
   const [subject, setSubject] = useState<subject[]>([]);
-  const [searchResult,setSearchResult] = useState<subject[]>([]);
+  const [searchResult, setSearchResult] = useState<subject[]>([]);
   const [focus, setFocus] = useState(false);
 
   const onClickSearch = () => {
-    const tmp_sbj = subject.filter((sbj)=>
-    {
-      const tmp = sbj.sbj_name.replace('_','');
-      return tmp.toLowerCase().includes(search.toLowerCase())
+    const tmp_sbj = subject.filter((sbj) => {
+      const tmp = sbj.sbj_name.replace("_", "");
+      return tmp.toLowerCase().includes(search.toLowerCase());
     });
     setSearchResult(tmp_sbj);
   };
 
-  const focusEvent = () => {
+  function onclick(str:String)
+  {
+    console.log(str)
+  }
 
-  };
+  const focusEvent = () => {};
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch(`http://localhost:3001/subjects`)
-    .then((res)=>res.json())
-    .then((data)=>setSubject(data));
-    },[]);
+      .then((res) => res.json())
+      .then((data) => setSubject(data));
+  }, []);
 
   return (
     <div className={styles.background}>
@@ -53,20 +54,31 @@ export default function Menu({intraId}:intraId) {
               type="text"
               placeholder="과제 검색하기"
               className={styles.form}
-              onFocus={()=>setFocus(!focus)}
-              onBlur={()=>setFocus(!focus)}
+              onFocus={() => setFocus(!focus)}
+              // onBlur={() => setFocus(!focus)}
               onChange={(e) => {
-                setSearch(e.target.value.replaceAll(' ','').replaceAll('_',''));
+                setSearch(
+                  e.target.value.replaceAll(" ", "").replaceAll("_", "")
+                );
                 onClickSearch();
               }}
             />
-            {focus && search != '' && <div className={styles.dropdown_content}>
-              <ul>
-                {
-                  searchResult.map((data, idx)=><li key={idx}><div><Link to={`/${data.circle}/${data.sbj_name}`}>{data.sbj_name}</Link></div></li>)
-                }
-              </ul>
-            </div>}
+            {focus && search && (
+              <div className={styles.dropdown_content}>
+                <ul>
+                  {searchResult.map((data, idx) => (
+                    <li key={idx}>
+                      <button onClick={()=>onclick(data.sbj_name)}>123</button>
+                      <div>
+                        <Link to={`/${data.circle}_circle/${data.sbj_name}`}>
+                          <div>{data.sbj_name}</div>
+                        </Link>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
           </form>
 
           <div>
