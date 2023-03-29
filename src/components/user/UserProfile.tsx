f4f78278648ab7d37ce61f00d35e546a032f8b65
HyeongwooIM <hyeongwoo1279@gmail.com>
2023-03-29 15:45:50 +0900

    [refacto] 풀 후 재커밋 #47

--- src/components/user/UserProfile.tsx
+++ src/components/user/UserProfile.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { profileState } from "../../utils/recoil/user";
-import styles from "./UserProfile.module.css"
+import styles from "./UserProfile.module.css";
 
 interface UserStatus {
   my_comment_num: Number;
@@ -10,7 +10,7 @@ interface UserStatus {
 
 export default function UserProfile() {
   const [userStatus, setUserStatus] = useState<UserStatus>();
-  const [userInfo, setUserInfo] = useRecoilState(profileState)
+  const [userInfo, setUserInfo] = useRecoilState(profileState);
 
   useEffect(() => {
     fetch(`http://localhost:3001/user_status`)
@@ -18,10 +18,18 @@ export default function UserProfile() {
       .then((data) => setUserStatus(data));
   }, []);
 
-  return <div className={styles.profile}>
-	  <div className={styles.profile}>{userInfo?.intraId}님 Lv.{userInfo.user_level}
-	  <button>로그아웃</button></div>
-	  <div className={styles.profile}><>내 후기 {userStatus?.my_comment_num} 개</></div>
-	  <div className={styles.profile}><>좋아요 {userStatus?.recommend_comment} 개</></div>
-	  </div>;
+  return (
+    <div className={styles.profile}>
+      <div className={styles.profile}>
+        {userInfo?.intraId}님 Lv.{userInfo.user_level}
+        <button>로그아웃</button>
+      </div>
+      <div className={styles.profile}>
+        <>내 후기 {userStatus?.my_comment_num} 개</>
+      </div>
+      <div className={styles.profile}>
+        <>좋아요 {userStatus?.recommend_comment} 개</>
+      </div>
+    </div>
+  );
 }
