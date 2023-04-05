fdbd38fcd8d41c545363eb891d63d4023f10dd2f
ppparkta <pppppark2@gmail.com>
2023-04-05 20:59:46 +0900

    refactor: 모바일 상세페이지 css 수정

--- src/components/subject_detail/SubjectInfo.tsx
+++ src/components/subject_detail/SubjectInfo.tsx
@@ -3,7 +3,7 @@ import styles from './SubjectInfo.module.css'
 export default function SubjectInfo() {
 	return (
 		<div className={styles.SubjectInfo}>
-			해당 과제는 쇼ㅏㄹ라쇼ㅏㄹ라
+			해당 과제는 매일 수십억 명의 온라인 사용자를 안전하게 지키고 사용하기 쉬운 개인정보 보호 설정을 통해 사용자를 보호하는 과제입니다.
 		</div>
 	)
 }
