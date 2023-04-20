936292d63b7abe013eae67b84bfabb810c5d8e79
ppparkta <pppppark2@gmail.com>
2023-04-21 01:52:07 +0900

    feat: #72 마이페이지 css 추가

--- src/components/myPage/MyPage.tsx
+++ src/components/myPage/MyPage.tsx
@@ -1,12 +1,11 @@
-import Menu from "../menu/Menu";
-import { profileState } from "../../utils/recoil/user";
-import { useRecoilState } from "recoil";
-import MyPageAvg from "./MyPageAvg";
-import styles from "./MyPage.module.css";
 import { useState, useEffect } from "react";
-import CommentList from "./CommentList";
-import WriteableList from "./WriteableList";
 import { useParams } from "react-router";
+import { useRecoilState } from "recoil";
+import { profileState } from "../../utils/recoil/user";
+import styles from "./MyPage.module.css";
+import Menu from "../menu/Menu";
+import WriteableList from "./WriteableList";
+import CommentList from "./CommentList";
 
 interface Profile {
   user_level: number;
@@ -14,15 +13,14 @@ interface Profile {
   recommend_comment: number;
 }
 
-
 export default function MyComment() {
   const [userState, setProfileState] = useRecoilState(profileState);
   const [contentState, setContentState] = useState<string>("writeableComment");
   const [profileUser, setProfileUser] = useState<any>({});
   const [myComments, setMyComments] = useState([]);
   const [likeComments, setLikeComments] = useState([]);
-  const [unreviewed, setUnreviewed] = useState([]);
-  const [unreviewedNumber, setUnreviewedNumber] = useState<number>(0);
+  const [unreviewed, setUnreviewed] = useState<any>([]);
+  const [unreviewedNumber, setUnreviewedNumber] = useState(0);
   const { intraId } = useParams();
 
   const menuName = userState.intraId === intraId ? "마이페이지" : "프로필";
@@ -33,58 +31,99 @@ export default function MyComment() {
     fetch(`${baseUrl}/user_status`)
       .then((res) => res.json())
       .then((data) => setProfileUser(data));
-  }, []);
-
-  useEffect(() => {
     fetch(`${baseUrl}/myComment`)
       .then((res) => res.json())
       .then((data) => setMyComments(data));
-  }, []);
-
-  useEffect(() => {
     fetch(`${baseUrl}/likeComment`)
       .then((res) => res.json())
       .then((data) => setLikeComments(data));
-  }, []);
-
-  useEffect(() => {
     fetch(`${baseUrl}/unreviewed`)
       .then((res) => res.json())
       .then((data) => setUnreviewed(data));
   }, []);
 
+  useEffect(() => {
+    const number = unreviewed.length;
+    setUnreviewedNumber(number);
+  }, [unreviewed]);
+
+  const UserState = () => (
+    <div className={styles.userState}>
+      <div>
+        <span className={styles.userId}>{intraId}</span>
+        <span className={styles.userLevel}>레벨{profileUser.user_level}</span>
+      </div>
+      <div>
+        <span className={styles.userUpdate}>회원정보 업데이트</span>
+      </div>
+    </div>
+  );
+
+  const MyPageAvg = () => (
+    <div className={styles.avg_container}>
+      <div className={styles.avg_box}>
+        <div>{unreviewedNumber}</div>
+        <div>작성 가능한 리뷰</div>
+      </div>
+      <div className={styles.avg_box}>
+        <div>{profileUser.my_comment_num}</div>
+        <div>내 리뷰</div>
+      </div>
+      <div className={styles.avg_box}>
+        <div>{profileUser.recommend_comment}</div>
+        <div>좋아요한 리뷰</div>
+      </div>
+    </div>
+  );
+
   return (
     <>
       <Menu menuName={menuName} />
       <div className={styles.container}>
-        <div>
-          <span>{intraId}</span>
-          <span>{profileUser.user_level}</span>
-          <span>회원정보 업데이트</span>
-        </div>
-        <div className={styles.line} />
+        <UserState />
         {menuName === "마이페이지" && <MyPageAvg />}
       </div>
-      <button onClick={() => setContentState("writeableComment")}>
-        작성 가능한 리뷰
-      </button>
-      <button onClick={() => setContentState("myComment")}>
-        {menuName === "마이페이지"
-          ? "내가 작성한 리뷰"
-          : `작성한 리뷰 ${profileUser.my_comment_num}`}
-      </button>
-      {menuName === "마이페이지" && (
-        <button onClick={() => setContentState("likeComment")}>
-          좋아요한 리뷰
+      <div className={styles.tab}>
+        <button
+          onClick={() => setContentState("writeableComment")}
+          className={
+            contentState === "writeableComment"
+              ? styles.tabBtn
+              : styles.tabBtnNo
+          }
+        >
+          작성 가능한 리뷰
         </button>
-      )}
-      <div>
-        {contentState === "writeableComment" && <WriteableList subject={unreviewed} />}
-        {contentState === "myComment" && <CommentList comments={myComments} isLikeComment={false}/>}
-        {contentState === "likeComment" && menuName === "마이페이지" && (
-          <CommentList comments={likeComments} isLikeComment={true}/>
+        <button
+          onClick={() => setContentState("myComment")}
+          className={
+            contentState === "myComment" ? styles.tabBtn : styles.tabBtnNo
+          }
+        >
+          {menuName === "마이페이지"
+            ? "내가 작성한 리뷰"
+            : `작성한 리뷰 ${profileUser.my_comment_num}`}
+        </button>
+        {menuName === "마이페이지" && (
+          <button
+            onClick={() => setContentState("likeComment")}
+            className={
+              contentState === "likeComment" ? styles.tabBtn : styles.tabBtnNo
+            }
+          >
+            좋아요한 리뷰
+          </button>
         )}
       </div>
+      {contentState === "writeableComment" && (
+        <WriteableList subject={unreviewed} />
+      )}
+      {contentState === "myComment" && (
+        <CommentList comments={myComments} isLikeComment={false} />
+      )}
+      {contentState === "likeComment" && menuName === "마이페이지" && (
+        <CommentList comments={likeComments} isLikeComment={true} />
+      )}
       <div>로그아웃</div>
     </>
   );
